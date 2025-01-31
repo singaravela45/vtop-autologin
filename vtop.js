e101a2f54d860c6e6a59f87c88e75875f8cea88d
Singara  vela <157232889+singaravela45@users.noreply.github.com>
2025-01-31 21:30:00 +0530

    Created javascript for autologin

---
+++ vtop.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    const username = data.vtop_username;  // Change to your credentials
+    const password = data.vtop_password;
+
+    const initialButton = document.querySelector('.btn.btn-primary.fw-bold');
+    const usernameField = document.querySelector('#username');
+    const passwordField = document.querySelector('#password');
+    const submitButton = document.querySelector('#submitBtn');
+    const captchaField = document.querySelector('#captchaStr');
+
+    const clickInitialButton = () => {
+        if (initialButton) {
+            initialButton.click();
+            setTimeout(fillLoginFormAndSubmit, 10);
+        } else {
+            fillLoginFormAndSubmit();
+        }
+    };
+
+    const fillLoginFormAndSubmit = () => {
+        if (usernameField && passwordField) {
+            usernameField.value = username;
+            passwordField.value = password;
+        }
+
+        let isSubmitted = false;
+
+        const checkCaptchaAndSubmit = () => {
+            if (!isSubmitted && submitButton) {
+                if (captchaField) {
+                    // If CAPTCHA field exists, wait until it's filled
+                    if (captchaField.value) {
+                        submitButton.click();
+                        isSubmitted = true;
+                    } else {
+                        setTimeout(checkCaptchaAndSubmit,10);  // Wait for the CAPTCHA to be filled
+                    }
+                } else {
+                    // If CAPTCHA is not present, submit the form directly
+                    submitButton.click();
+                    isSubmitted = true;
+                }
+            }
+        };
+
+        checkCaptchaAndSubmit();
+    };
+
+    clickInitialButton();
+})();
