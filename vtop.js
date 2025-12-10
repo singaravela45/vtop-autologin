4f01b0ae8abeb8b0a05381bd4b57014fcedb65ac
Singara  vela R <157232889+singaravela45@users.noreply.github.com>
2025-12-10 09:15:22 +0530

    Refactor vtop.js for cleaner login process

--- vtop.js
+++ vtop.js
@@ -1,52 +1,51 @@
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    const username = data.vtop_username;  // Change to your credentials
-    const password = data.vtop_password;
+  const username = "your username"; // Change to your credentials
+  const password = "your password";
+  const initialButton = document.querySelector(".btn.btn-primary.fw-bold");
+  const usernameField = document.querySelector("#username");
+  const passwordField = document.querySelector("#password");
+  const submitButton = document.querySelector(".btn-sm");
+  const captchaField = document.querySelector("#captchaStr");
 
-    const initialButton = document.querySelector('.btn.btn-primary.fw-bold');
-    const usernameField = document.querySelector('#username');
-    const passwordField = document.querySelector('#password');
-    const submitButton = document.querySelector('#submitBtn');
-    const captchaField = document.querySelector('#captchaStr');
+  const clickInitialButton = () => {
+    if (initialButton) {
+      initialButton.click();
+      setTimeout(fillLoginFormAndSubmit, 10);
+    } else {
+      fillLoginFormAndSubmit();
+    }
+  };
 
-    const clickInitialButton = () => {
-        if (initialButton) {
-            initialButton.click();
-            setTimeout(fillLoginFormAndSubmit, 10);
+  const fillLoginFormAndSubmit = () => {
+    if (usernameField && passwordField) {
+      usernameField.value = username;
+      passwordField.value = password;
+    }
+
+    let isSubmitted = false;
+
+    const checkCaptchaAndSubmit = () => {
+      if (!isSubmitted && submitButton) {
+        if (captchaField) {
+          // If CAPTCHA field exists, wait until it's filled
+          if (captchaField.value) {
+            submitButton.click();
+            isSubmitted = true;
+          } else {
+            setTimeout(checkCaptchaAndSubmit, 10); // Wait for the CAPTCHA to be filled
+          }
         } else {
-            fillLoginFormAndSubmit();
+          // If CAPTCHA is not present, submit the form directly
+          submitButton.click();
+          isSubmitted = true;
         }
+      }
     };
 
-    const fillLoginFormAndSubmit = () => {
-        if (usernameField && passwordField) {
-            usernameField.value = username;
-            passwordField.value = password;
-        }
+    checkCaptchaAndSubmit();
+  };
 
-        let isSubmitted = false;
-
-        const checkCaptchaAndSubmit = () => {
-            if (!isSubmitted && submitButton) {
-                if (captchaField) {
-                    // If CAPTCHA field exists, wait until it's filled
-                    if (captchaField.value) {
-                        submitButton.click();
-                        isSubmitted = true;
-                    } else {
-                        setTimeout(checkCaptchaAndSubmit,10);  // Wait for the CAPTCHA to be filled
-                    }
-                } else {
-                    // If CAPTCHA is not present, submit the form directly
-                    submitButton.click();
-                    isSubmitted = true;
-                }
-            }
-        };
-
-        checkCaptchaAndSubmit();
-    };
-
-    clickInitialButton();
+  clickInitialButton();
 })();
