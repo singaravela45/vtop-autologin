214b474c4dde6a3a3a6f3d9d0fd68f9a52b9836c
Singaravela45 <singaravelaramesh.com>
2026-04-29 09:01:19 +0530

    update

--- vtop.js
+++ vtop.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
 
-  const username = "your username"; // Change to your credentials
-  const password = "your password";
+  const username = "7092624365SING"; // Change to your credentials
+  const password = "Vtop@@24365";
   const initialButton = document.querySelector(".btn.btn-primary.fw-bold");
   const usernameField = document.querySelector("#username");
   const passwordField = document.querySelector("#password");
